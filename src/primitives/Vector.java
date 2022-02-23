2c7229e934deb76a30fb666b13f77fcdcc1827ef
idanmaman2 <90776557+idanmaman2@users.noreply.github.com>
2022-02-23 12:41:57 +0200

    Double 3 operations

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -5,9 +5,10 @@ import java.awt.*;
 public class Vector extends Point {
 
     public Vector (Point pt1 , Point pt2 ) { //from pt1 to pt2  -> pt1pt2 vector
-        super(pt2.xyz.d1 - pt1.xyz.d1 ,
-                pt2.xyz.d2 - pt1.xyz.d2,
-                pt2.xyz.d3 - pt1.xyz.d3 );
+        super(pt1.xyz.subtract(pt2.xyz));
+    }
+    public Vector(Double3 db3){
+        super(db3);
     }
     public Vector(double d1, double d2, double d3) {
         super(d1, d2, d3);
@@ -29,16 +30,11 @@ public class Vector extends Point {
     }
 
     public Vector add(Vector vc) {
-        return new Vector(this.xyz.d1 + vc.xyz.d1,
-                this.xyz.d2 + vc.xyz.d2,
-                this.xyz.d3 + vc.xyz.d3);
+        return new Vector(this.xyz.add(vc.xyz));
     }
 
     public Vector scale(double sc) {
-        return new Vector(sc * this.xyz.d1,
-                sc * this.xyz.d2,
-                sc * this.xyz.d3
-        );
+      return new Vector( this.xyz.scale(sc));
     }
 
     public double dotProduct(Vector vc) {
@@ -57,10 +53,7 @@ public class Vector extends Point {
     }
 
     public Vector normalize(){
-        double length = this.length() ;
-        return (length == 1  ?
-                this  :
-                this.scale(1/length));
+        return this.scale(1/this.length());
     }
     /*
      * (x1,y1,z1) ( x2,y2,z2)
